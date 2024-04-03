78bf02d4a9c1ffdcbf458e752a8de08c1c308a13
Ali ERBEN <aerben@amiltone.com>
2024-04-03 18:07:25 +0200

    ajout du TcpHandler

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using YourNamespace;
+
+namespace ProjetBadgeBackEnd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpClientHandler clientHandler = new TcpClientHandler();
+            clientHandler.ConnectToAllOpenPort();
+
+            Console.WriteLine("\nAppuyez sur Entrée pour continuer...");
+            Console.Read();
+        }
+    }
+}
